a8bca1cffff551355795ab5447bbacad1a58e96c
Nikolai Ivanov <gravitycats@gmail.com>
2015-09-05 18:01:04 +0300

    Layed down some styles and content

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,71 @@
+var health = 2;
 $(document).ready(function() {
-        $('.first').show();
-        $('.first>#answers>.answer').click(function() {
-            $('.first').hide();
-            $('.second').show();
+    $('.first').show();
+    $('.first>#answers>p').not(document.getElementById('answer')).click(function() {
+        health -= 1;
+        if (health <= 0) {
+            $('#wrapper>img:nth-of-type(2)').show();
+        };
+    });
+    $('.first>#answers>#answer').click(function() {
+        $('.first').remove();
+        $('.second').show();
+        $('.second>#answers>p').not(document.getElementById('answer')).click(function() {
+            health -= 1;
+            if (health <= 0) {
+                $('#wrapper>img:nth-of-type(2)').show();
+            };
         });
-        $('.second>#answers>.answer').click(function() {
-            $('.second').hide();
-            $('.third').show();
-           $('#wrapper>img').attr('src', 'images/met-2.png');
+    });
+    $('.second>#answers>#answer').click(function() {
+        $('.second').remove();
+        $('.third').show();
+        $('.third>#answers>p').not(document.getElementById('answer')).click(function() {
+            health -= 1;
+            if (health <= 0) {
+                $('#wrapper>img:nth-of-type(2)').show();
+            };
         });
-        $('.third>#answers>.answer').click(function() {
-            $('.third').hide();
-            $('.fourth').show();
+    });
+    $('.third>#answers>#answer').click(function() {
+        $('.third').remove();
+        $('.fourth').show();
+        $('.fourth>#answers>p').not(document.getElementById('answer')).click(function() {
+            health -= 1;
+            if (health <= 0) {
+                $('#wrapper>img:nth-of-type(2)').show();
+            };
         });
-        $('.fourth>#answers>.answer').click(function() {
-            $('.fourth').hide();
-            $('.fifth').show();
+    });
+    $('.fourth>#answers>#answer').click(function() {
+        $('.fourth').remove();
+        $('.fifth').show();
+        $('.fifth>#answers>p').not(document.getElementById('answer')).click(function() {
+            health -= 1;
+            if (health <= 0) {
+                $('#wrapper>img:nth-of-type(2)').show();
+            };
         });
-        $('.fifth>#answers>.answer').click(function() {
-            $('.fifth').hide();
-            $('.sixth').show();
-           $('#wrapper>img').attr('src', 'images/met-1.png');
+    });
+    $('.fifth>#answers>#answer').click(function() {
+        $('.fifth').remove();
+        $('.sixth').show();
+        $('.sixth>#answers>p').not(document.getElementById('answer')).click(function() {
+            health -= 1;
+            if (health <= 0) {
+                $('#wrapper>img:nth-of-type(2)').show();
+            };
         });
+    });
+    // $('article>#answers>p').not(document.getElementById('answer')).click(function() {
+    //     health -= 1;
+    //     console.log(health);
+    //     if (health <= 0) {
+    //         $('#wrapper>img:nth-of-type(2)').show();
+    //     };
+    // });
+
+
 });
 // var QACount = 1;
 // $(document).ready(function() {
