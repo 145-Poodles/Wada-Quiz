2666ae8dc9dfa117f13661299ef2a5ac05cf20b1
Nikolai Ivanov <gravitycats@gmail.com>
2015-09-05 20:26:14 +0300

    Finalized game styles and background content

--- scripts/script.js
+++ scripts/script.js
@@ -3,99 +3,162 @@ $(document).ready(function() {
     $('.first').show();
     $('.first>#answers>p').not(document.getElementById('answer')).click(function() {
         health -= 1;
+        $('.wrong').show();
+        setTimeout(function() {
+            $('.wrong').hide();
+        }, 500);
         if (health <= 0) {
             $('#wrapper>img:nth-of-type(2)').show();
+            $('.memoca').hide();
+            $('.dolphi').hide();
+            $('article').hide();
+            $('.blue-border').hide();
+            $('body').css('background', 'url("./images/red-tile.png")');
+            $('#wrapper>img:nth-of-type(1)').removeClass('blue-border');
+            $('#wrapper>img:nth-of-type(1)').addClass('red-border');
+            $('#wrapper>.lost').css('display', 'block');
+            $('.stars').css('background', 'url("./images/red-stars.png")');
+            $('.logo').attr('src', './images/red-logo.png');
         };
     });
     $('.first>#answers>#answer').click(function() {
         $('.first').remove();
         $('.second').show();
+        $('.correct').show();
+        setTimeout(function() {
+            $('.correct').hide();
+        }, 500);
         $('.second>#answers>p').not(document.getElementById('answer')).click(function() {
             health -= 1;
+            $('.wrong').show();
+            setTimeout(function() {
+                $('.wrong').hide();
+            }, 500);
             if (health <= 0) {
                 $('#wrapper>img:nth-of-type(2)').show();
+                $('.memoca').hide();
+                $('.dolphi').hide();
+                $('article').hide();
+                $('.blue-border').hide();
+                $('body').css('background', 'url("./images/red-tile.png")');
+                $('#wrapper>img:nth-of-type(1)').removeClass('blue-border');
+                $('#wrapper>img:nth-of-type(1)').addClass('red-border');
+                $('#wrapper>.lost').css('display', 'block');
+                $('.stars').css('background', 'url("./images/red-stars.png")');
+                $('.logo').attr('src', './images/red-logo.png');
             };
         });
     });
     $('.second>#answers>#answer').click(function() {
         $('.second').remove();
         $('.third').show();
+        $('.correct').show();
+        setTimeout(function() {
+            $('.correct').hide();
+        }, 500);
         $('.third>#answers>p').not(document.getElementById('answer')).click(function() {
             health -= 1;
+            $('.wrong').show();
+            setTimeout(function() {
+                $('.wrong').hide();
+            }, 500);
             if (health <= 0) {
                 $('#wrapper>img:nth-of-type(2)').show();
+                $('.memoca').hide();
+                $('.dolphi').hide();
+                $('article').hide();
+                $('.blue-border').hide();
+                $('body').css('background', 'url("./images/red-tile.png")');
+                $('#wrapper>img:nth-of-type(1)').removeClass('blue-border');
+                $('#wrapper>img:nth-of-type(1)').addClass('red-border');
+                $('#wrapper>.lost').css('display', 'block');
+                $('.stars').css('background', 'url("./images/red-stars.png")');
+                $('.logo').attr('src', './images/red-logo.png');
             };
         });
     });
     $('.third>#answers>#answer').click(function() {
         $('.third').remove();
         $('.fourth').show();
+        $('.correct').show();
+        setTimeout(function() {
+            $('.correct').hide();
+        }, 500);
         $('.fourth>#answers>p').not(document.getElementById('answer')).click(function() {
             health -= 1;
+            $('.wrong').show();
+            setTimeout(function() {
+                $('.wrong').hide();
+            }, 500);
             if (health <= 0) {
                 $('#wrapper>img:nth-of-type(2)').show();
+                $('.memoca').hide();
+                $('.dolphi').hide();
+                $('article').hide();
+                $('.blue-border').hide();
+                $('body').css('background', 'url("./images/red-tile.png")');
+                $('#wrapper>img:nth-of-type(1)').removeClass('blue-border');
+                $('#wrapper>img:nth-of-type(1)').addClass('red-border');
+                $('#wrapper>.lost').css('display', 'block');
+                $('.stars').css('background', 'url("./images/red-stars.png")');
+                $('.logo').attr('src', './images/red-logo.png');
             };
         });
     });
     $('.fourth>#answers>#answer').click(function() {
         $('.fourth').remove();
         $('.fifth').show();
+        $('.correct').show();
+        setTimeout(function() {
+            $('.correct').hide();
+        }, 500);
         $('.fifth>#answers>p').not(document.getElementById('answer')).click(function() {
             health -= 1;
+            $('.wrong').show();
+            setTimeout(function() {
+                $('.wrong').hide();
+            }, 500);
             if (health <= 0) {
                 $('#wrapper>img:nth-of-type(2)').show();
+                $('.memoca').hide();
+                $('.dolphi').hide();
+                $('article').hide();
+                $('.blue-border').hide();
+                $('body').css('background', 'url("./images/red-tile.png")');
+                $('#wrapper>img:nth-of-type(1)').removeClass('blue-border');
+                $('#wrapper>img:nth-of-type(1)').addClass('red-border');
+                $('#wrapper>.lost').css('display', 'block');
+                $('.stars').css('background', 'url("./images/red-stars.png")');
+                $('.logo').attr('src', './images/red-logo.png');
             };
         });
     });
     $('.fifth>#answers>#answer').click(function() {
         $('.fifth').remove();
         $('.sixth').show();
+        $('.correct').show();
+        setTimeout(function() {
+            $('.correct').hide();
+        }, 500);
         $('.sixth>#answers>p').not(document.getElementById('answer')).click(function() {
             health -= 1;
+            $('.wrong').show();
+            setTimeout(function() {
+                $('.wrong').hide();
+            }, 500);
             if (health <= 0) {
                 $('#wrapper>img:nth-of-type(2)').show();
+                $('.memoca').hide();
+                $('.dolphi').hide();
+                $('article').hide();
+                $('.blue-border').hide();
+                $('body').css('background', 'url("./images/red-tile.png")');
+                $('#wrapper>img:nth-of-type(1)').removeClass('blue-border');
+                $('#wrapper>img:nth-of-type(1)').addClass('red-border');
+                $('#wrapper>.lost').css('display', 'block');
+                $('.stars').css('background', 'url("./images/red-stars.png")');
+                $('.logo').attr('src', './images/red-logo.png');
             };
         });
     });
-    // $('article>#answers>p').not(document.getElementById('answer')).click(function() {
-    //     health -= 1;
-    //     console.log(health);
-    //     if (health <= 0) {
-    //         $('#wrapper>img:nth-of-type(2)').show();
-    //     };
-    // });
-
-
 });
-// var QACount = 1;
-// $(document).ready(function() {
-//     if (QACount == 1) {
-//         $('.first').show();
-//         $('.first>#answers>.answer').click(function() {
-//             $('.first').hide();
-//             $('.second').show();
-//             QACount += 1;
-//             console.log(QACount);
-//             if (QACount == 2) {
-//                 $('.second>#answers>.answer').click(function() {
-//                     $('.second').hide();
-//                     $('.third').show();
-//                     QACount += 1;
-//             console.log(QACount);
-//                 });
-//             };
-//             if (QACount == 3) {
-//                 $('.third>#answers>.answer').click(function() {
-//                     $('.third').hide();
-//                     $('.fourth').show();
-//                     QACount += 1;
-//             console.log(QACount);
-//                 });
-//             };
-//             if (QACount == 4) {
-//                 console.log('You win!');
-//                 $('#wrapper>img').attr('src', 'images/met-1.png');
-//             };
-//         });
-//     };
-// });
