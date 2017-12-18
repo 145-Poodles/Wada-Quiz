32c9e7c52117f1678142cac12ff671f1dabe4618
Nikolay Ivanov <niki.ivanov98@gmail.com>
2017-12-18 18:45:21 +0100

    Completed the quiz walkthrough and added a backdrop

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,6 +1,53 @@
+// These are all the questions for
+// the user to answer in the quiz.
 let levels = [{
     question: 'Who was Wadanohara\'s first familiar?',
     choices: ['Memoca', 'Dolphi', 'Samekichi', 'Fukami'],
     backgroundPath: './images/bg-2.jpg',
     answer: 'Samekichi'
+},{
+    question: 'Who is Wadanohara\'s tutor?',
+    choices: ['Tatsumiya', 'Meikai', 'Princess', 'Chlomaki'],
+    backgroundPath: './images/bg-2.jpg',
+    answer: 'Tatsumiya'
+},{
+    question: 'What instrument does Wadanohara play the most?',
+    choices: ['Piano', 'Flute', 'Ocarina', 'Guitar'],
+    backgroundPath: './images/bg-2.jpg',
+    answer: 'Ocarina'
+},{
+    question: 'From who did Wadanohara aquire the ocarina?',
+    choices: ['Samekichi', 'Chlomaki', 'Tatsumiya', 'Fukami'],
+    backgroundPath: './images/smug.png',
+    answer: 'Samekichi'
+},{
+    question: 'How many witches are seen throught the game?',
+    choices: ['2', '3', '4', '5'],
+    backgroundPath: './images/smug.png',
+    answer: '4'
+},{
+    question: 'Why did Tosatsu Kingdom invade the Sea Kingdom?',
+    choices: ['They were tricked by the Sea of Death to attack the Deep-Sea.', 'The Sea Kingdom killed rabbits to provoke the Princess.'],
+    backgroundPath: './images/The_greatest_moment.png',
+    answer: 'They were tricked by the Sea of Death to attack the Deep-Sea.'
+},{
+    question: 'Which demon was Wadanohara friends with?',
+    choices: ['Laurentia', 'Ver Million', 'Vendetto', 'Sal'],
+    backgroundPath: './images/The_greatest_moment.png',
+    answer: 'Ver Million'
+},{
+    question: 'Why does Artamos look like a robot?',
+    choices: ['He was cursed by a witch, thus having his form changed.', 'He was defeated in a battle and forced to change.'],
+    backgroundPath: './images/cute.png',
+    answer: 'He was cursed by a witch, thus having his form changed.'
+},{
+    question: 'Where else does Chlomaki appear in?',
+    choices: ['Ice Scream', 'Mogeko Castle', 'Grey Garden', 'Obsolete Dream'],
+    backgroundPath: './images/cute.png',
+    answer: 'Obsolete Dream'
+},{
+    question: 'Who is Wadanohara\'s Mom? :D?',
+    choices: ['Princess Uomi', 'Chlomaki', 'Great Witch', 'The Moon'],
+    backgroundPath: './images/cute.png',
+    answer: 'The Moon'
 }];
